afaee4b1d1029cf2be1d1d4c61f3c5ebe4b3bbeb
zanaw <zuznawtoik@gmail.com>
2021-10-15 20:20:28 +0200

    user commit

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 const _=require('lodash');
 
-function srednia() {
-    console.log(_.mean(arguments));
+const user = {
+    name: 'Imie',
+    surname: 'Nazwisko',
+    allGrades: [
+        {
+            subjectName: 'Name1',
+            grades: [5,4,3,2],
+            weight: 3
+        },
+        {
+            subjectName: 'Name2',
+            grades: [3,3.5,2],
+            weight: 1
+        },
+        {
+            subjectName: 'Name1',
+            grades: [4,3,3.5],
+            weight: 5
+        }
+    ]
 }
 
-srednia(1, 2, 3, 4, 5);
-srednia(2, 4, 6);
+function wmean(user){
+
+    var swynik = 0;
+    var wynik = 0;
+
+    swynik=(_.sum(user.allGrades[0].grades))*user.allGrades[0].weight+(_.sum(user.allGrades[1].grades))*user.allGrades[1].weight
+        +(_.sum(user.allGrades[2].grades))*user.allGrades[2].weight;
+    wynik=swynik/((user.allGrades[0].grades.length*user.allGrades[0].weight)+(user.allGrades[1].grades.length*user.allGrades[1].weight)
+        +(user.allGrades[2].grades.length*user.allGrades[2].weight));
+
+    console.log(user.name+" "+user.surname+" "+wynik);
+}
+
+wmean(user);
